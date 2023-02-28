ff6768b130f81d769fdf9e041598aab35c2702e0
mirroringcode <miguelolivar999@gmail.com>
2023-02-28 14:46:53 -0400

    añadida funcionalidad de ver repos de usuario

--- js/main.js
+++ js/main.js
@@ -10,6 +10,37 @@ $(document).ready(function() {
     		client_secret:'1f7c17a03c74dad058e7f58e3f7652d171e2a8c0'
     	}
     }).done(function(user) {
+		$.ajax({
+			url:`https://api.github.com/users/${username}/repos`,
+			data:{
+				client_id:'03ee0a6cb8863b925aa5',
+				client_secret:'1f7c17a03c74dad058e7f58e3f7652d171e2a8c0',
+				sort: 'created: asc',
+				per_page: 5
+			}
+		}).done(function(repos) {
+			$.each(repos, function(index, repo) {
+				$('#repos').append(`
+					<div class="well">
+						<div class="row">
+							<div class="col-md-7">
+								<strong>${repo.name}</strong>: ${repo.description}
+							</div>
+
+							<div class="col-md-3">
+								<span class='badge badge-primary'>Forks: ${repo.forks_count}</span>
+								<span class='badge badge-secondary'>Watchers: ${repo.watchers_count}</span>
+								<span class='badge badge-success'>Stars: ${repo.stargazers_count}</span>
+							</div>
+
+							<div class="col-md-2">
+								<a href="${repo.html_url}" target="_blank" class="btn btn-primary">Repo page</a>
+							</div>
+						</div>
+					</div>
+				`)
+			});
+		});
     	$('#profile').html(`
 
     		<div class='card card-default'>
@@ -29,12 +60,19 @@ $(document).ready(function() {
     					<span class='badge badge-secondary'>Public Gists: ${user.public_gists}</span>
     					<span class='badge badge-success'>Followers: ${user.followers}</span>
     					<span class='badge badge-success'>Following: ${user.following}</span>
-    					
+    					<br></br>
+						<ul class="list-group">
+							<li class="list-group-item">Company: ${user.company}</li>
+							<li class="list-group-item">Website / Blog: ${user.blog}</li>
+							<li class="list-group-item">Location: ${user.location}</li>
+							<li class="list-group-item">Member since: ${user.created_at}</li>
+						</ul>
     					</div>
     				</div>
     			</div>
     		</div>
-
+			<h3 class="page-header">latest repos</h3>
+			<div id="repos"></div>
     		`)
     });
     
